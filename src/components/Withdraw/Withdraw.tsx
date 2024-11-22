1fb3eebb2cba75432c7befd7c14ec5c3430f107b
harshsoni071 <harshsoni8118@gmail.com>
2024-11-22 11:57:41 +0530

    update

--- src/components/Withdraw/Withdraw.tsx
+++ src/components/Withdraw/Withdraw.tsx
@@ -5,9 +5,73 @@ import Footer from "../Common/Footer";
 function Withdraw() {
   return (
     <>
-      <section className="w-[400px] min-h-[100vh] h-auto bg-blue-400 mx-auto relative">
-        <Header title="Withdraw" />
-        <div className="p-4">
+      <section className="w-[400px] min-h-[100vh] h-auto bg-white mx-auto relative">
+        <Header title="My Wallet" />
+
+        <div className="bg-blue-500 h-[20vh] pt-12">
+          <h2 className="text-center text-white text-[14px]">
+            Money in your Wallet
+          </h2>
+        </div>
+        <div className="border mx-3 px-3 pt-4 pb-9 m-[-35px] bg-white relative">
+          <h2 className="text-center font-semibold text-[17px]">
+            Withdraw Money
+          </h2>
+          <form action="#" className="py-2">
+            <div className="flex flex-col my-2">
+              <label className="text-blue-600 font-semibold text-[14px]">
+                Enter Amount ($)
+              </label>
+              <input
+                type="text"
+                placeholder="1000"
+                className="border px-2 py-2 rounded-md my-2"
+              />
+            </div>
+            <div className="flex flex-col my-2">
+              <label className="text-blue-600 font-semibold text-[14px]">
+                UPI ID
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Your UPI ID"
+                className="border px-2 py-2 rounded-md my-2"
+              />
+            </div>
+          </form>
+          <div>
+            <div className="flex justify-between pb-2">
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                1000
+              </button>
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                2000
+              </button>
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                3000
+              </button>
+            </div>
+            <div className="flex justify-between pt-2">
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                4000
+              </button>
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                5000
+              </button>
+              <button className="text-blue-600 font-semibold border-[1px] border-blue-600 w-[100px] py-[3px] rounded-full hover:bg-blue-600 hover:text-white">
+                10000
+              </button>
+            </div>
+          </div>
+          <div className="absolute bottom-[-20px] left-24">
+          <button className="bg-blue-600 text-white text-[13px] font-semibold py-[7px] px-9 rounded-full hover:text-blue-600 hover:bg-white hover:border-[1px] hover:border-blue-600">
+            PROCEED To ADD
+          </button>
+        </div>
+        </div>
+        
+
+        {/* <div className="p-4">
           <div className="pt-4">
             <p className="text-[20px] text-white font-semibold">Balance</p>
             <span className="text-[22px] text-white font-semibold">
@@ -41,7 +105,6 @@ function Withdraw() {
             Withdraw
           </button>
         </div>
-
         <div className="px-3 pt-4  bg-neutral-400 text-center">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
@@ -52,8 +115,8 @@ function Withdraw() {
               "9929393939" Number
             </button>
           </div>
-        </div>
-        <Footer />
+        </div> */}
+        {/* <Footer /> */}
       </section>
     </>
   );
